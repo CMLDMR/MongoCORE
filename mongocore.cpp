03f2dadb09cfc57a6cff530e4dd5d483830ca44f
Cemal Demir <CMLDMR@users.noreply.github.com>
2023-02-17 10:10:06 +0300

    MongoCore

---
+++ mongocore.cpp
@@ -0,0 +1,8 @@
+
+#include "mongocore.h"
+
+
+MongoCore::MongoCore()
+{
+}
+
---
+++ mongocore.h
@@ -0,0 +1,15 @@
+
+#ifndef MONGOCORE_H
+#define MONGOCORE_H
+
+#include "MongoCore_global.h"
+
+
+class MONGOCORE_EXPORT MongoCore
+{
+public:
+    MongoCore();
+};
+
+
+#endif // MONGOCORE_H
